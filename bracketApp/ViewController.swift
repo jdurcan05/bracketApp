70305077846e7c1707d98f01c8fd8746e7e362eb
James Durcan <jdurcan05@gmail.com>
2022-12-02 07:42:19 -0600

    Added important variable

--- bracketApp/ViewController.swift
+++ bracketApp/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     var test = "test"
     var kanye = "Not cool dude"
+    var mason = "Sucks"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
