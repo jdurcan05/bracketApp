b13cf9fb2a9c8ba1ba1b9d60ab3f9efdf29e7e9a
Mason <mason.rozumny23@d155.org>
2022-12-02 08:06:43 -0600

    adding teams to an array

--- bracketApp/TeamScreen.swift
+++ bracketApp/TeamScreen.swift
@@ -9,16 +9,29 @@ import UIKit
 
 class TeamScreen: UIViewController {
 
+    @IBOutlet weak var textfieldOutlet: UITextField!
+    
+    var teams: [String] = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func textFieldOutlet(_ sender: Any) {
-    }
-    
-    @IBAction func plusButtonAction(_ sender: Any) {
+    @IBAction func plusButtonAction(_ sender: Any)
+    {
+        if textfieldOutlet.text != nil
+        {
+            teams.append(textfieldOutlet.text!)
+        }
+        else
+        {
+            let alert = UIAlertController(title: "Error", message: "Enter a team name!", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .destructive, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        }
+        
     }
     
     @IBAction func confirmButtonAction(_ sender: Any) {
